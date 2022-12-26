a2a1ce72e602b92231ac88b277fcc6f4be20bb32
seongjo24 <kangsj24@naver.com>
2022-12-26 13:55:21 +0900

    학습테스트 실습

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,53 @@
+package study;
+
+import org.assertj.core.internal.Strings;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.platform.commons.util.StringUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.*;
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("set 크기확인")
+    void test1(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("set cotains")
+    void test2(){
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  "})
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(String input) {
+        assertTrue(StringUtils.isBlank(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"test:test", "tEst:test", "Java:java"}, delimiter = ':')
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(String input, String expected) {
+        String actualValue = input.toLowerCase();
+        assertEquals(expected, actualValue);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,4 +12,40 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split1(){
+        String[] actualArr = "1,2".split(",");
+        assertThat(actualArr).contains("1");
+        assertThat(actualArr).containsExactly("1","2");
+    }
+
+    @Test
+    void split2(){
+        String[] actualArr = "1".split(",");
+        assertThat(actualArr).contains("1");
+        assertThat(actualArr).containsExactly("1");
+    }
+
+    @Test
+    void subString(){
+        String actualArr = "(1,2)".substring(1,4);
+        assertThat(actualArr).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt(){
+        String actualArr = "(1,2)".substring(1,4);
+        assertThat(actualArr).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("StringIndexOutOfBoundsException test")
+    void test(){
+        assertThatThrownBy(()->{
+            char a = "(1,2)".charAt(1);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
+
 }
